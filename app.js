c7538345a46efa1415753a1b7a16d71e9eb732f5
H4rolDev <harolsd121@gmail.com>
2025-01-24 12:00:44 -0500

    Cambios de comentarios en JS

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 let amigos = '';
 let nombres = [];
-let nuevaEtiquetaLI = '';
-let elementoHTML = '';
 
 function agregarAmigo(){
     amigos = document.getElementById('amigo').value;
@@ -19,19 +17,11 @@ function agregarAmigo(){
 function mostrarAmigos(){
     let lista = document.getElementById('listaAmigos');
     let contenido = '';
-
     for (let index = 0; index < nombres.length; index++) {
         // document.getElementById('listaAmigos').innerHTML = `<li>${nombres[index]}</li>`;
         contenido += `<li>${nombres[index]}</li>`
     }
-
     lista.innerHTML = contenido;
-    
-    // nuevaEtiquetaLI = document.createElement('li');
-    // // Agregar el nombre a la nueva etiqueta de lista
-    // nuevaEtiquetaLI.textContent = amigos;
-    // // Agregar la nueva etiqueta de lista al elemento HTML
-    // elementoHTML.appendChild(nuevaEtiquetaLI);
 }
 
 function limpiarCajaTexto() {
@@ -44,8 +34,5 @@ function sortearAmigo(){
 }
 
 function asignarTextoElemento(etiqueta, texto){
-    // Etiqueta es la etiqueta HTML a la que se le asignará el texto 
-    // y texto es el texto que se le asignará a la etiqueta
-    // textContent es una propiedad que permite asignar texto a una etiqueta HTML
     document.querySelector(etiqueta).textContent = texto;
 }
